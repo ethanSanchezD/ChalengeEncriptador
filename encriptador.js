560a0fc9a6e9380637c739768d7a76cfa299cd7a
ethanSanchezD <ethand.san@gmail.com>
2023-04-16 20:06:54 -0600

    added the logic for the image in the encrypt function

--- encriptador.js
+++ encriptador.js
@@ -1,10 +1,15 @@
 let x = document.getElementById("output");
 x.style.display = 'none';
 function encrypt() {
+    const image = document.getElementById("image");
+    image.style.display = 'none';
+
     x = document.getElementById("output");
     if (x.style.display === 'none') {
         x.style.display = 'block';
     } else {
+        image.style.display = 'block';
         x.style.display = 'none';
     }
 }
+
