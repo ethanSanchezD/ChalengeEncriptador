0cf0b2b7b3ba38f256da09f8777719fe33c33a25
ethanSanchezD <ethand.san@gmail.com>
2023-04-17 23:47:47 -0600

    functions to enconde and decode

--- encoder.js
+++ encoder.js
@@ -1,25 +1,62 @@
 
-function encode() {
-    let output = document.getElementById("output");
+function encodeClick() {
+    let outputMessage = document.getElementById("output-message");
     const noTextMessage = document.getElementById("no-text-message");
     const image = document.getElementById("image");
-    const message = document.getElementById("encode-message");
+    const messageInput = document.getElementById("encode-message");
+    let copyButton = document.getElementById("copy");
+    const coppyDiv = document.getElementById("div-of-copy");
 
-    output.style.display = 'none';
+    outputMessage.style.display = 'none';
     noTextMessage.style.display = 'none';
     image.style.display = 'none';
-    output = document.getElementById("output");
-    console.log(message.value);
-    if ((message.value == "") || (output.style.display != "none")) {
-        output.style.display = 'none';
+    outputMessage = document.getElementById("output-message");
+    copyButton = document.getElementById("copy");
+
+
+
+
+    if ((messageInput.value == "") || (outputMessage.style.display != "none")) {
+        outputMessage.style.display = 'none';
+        copyButton.style.display = 'none';
+        coppyDiv.style.display = 'none';
         noTextMessage.style.display = 'block';
         image.style.display = 'block';
     } else {
-        output.style.display = 'block';
-        output.style.alignContent =
-            image.style.display = 'none';
+        outputMessage.style.display = 'block';
+        copyButton.style.display = 'block';
+        coppyDiv.style.display = 'flex';
+        image.style.display = 'none';
         noTextMessage.style.display = 'none';
 
     }
+    encode();
+}
+
+function encode() {
+    let outputMessage = document.getElementById("output-message");
+    const messageInput = document.getElementById("encode-message");
+
+    outputMessage.value = messageInput.value.replaceAll("e", "enter").replaceAll("i", "imes").replaceAll("a", "ai").replaceAll("o", "ober").replaceAll("u", "ufat");
+
+
+
+
+}
+
+function decode() {
+    let outputMessage = document.getElementById("output-message");
+    const messageInput = document.getElementById("encode-message");
+
+    outputMessage.value = messageInput.value.replaceAll("enter", "e").replaceAll("imes", "i").replaceAll("ai", "a").replaceAll("ober", "o").replaceAll("ufat", "u");
+}
+
+function copy() {
+    let outputMessage = document.getElementById("output-message");
+
+    outputMessage.select();
+    document.execCommand("copy");
+
+
 }
 
